9daa3c2416a4aac86c52447bb2b0edfd6f74fb2f
N01im1T <70130976+N01im1T@users.noreply.github.com>
2024-03-11 19:47:31 +1000

    sc-commit

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import store from '../store/store';
-import CryptoList from './CryptoList';
+import store from './store/store';
+import CryptoList from './src/CryptoList';
 
 const App = () => {
   return (
--- src/CryptoList.js
+++ src/CryptoList.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { View, Text, ActivityIndicator } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, Image,  ActivityIndicator } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchCryptocurrencies } from '../store/actions/cryptoActions';
 
@@ -20,15 +20,36 @@ const CryptoList = () => {
   }
 
   return (
-    <View>
-      {cryptocurrencies.map((crypto) => (
-        <View key={crypto.id}>
-          <Text>{crypto.name}</Text>
-          <Text>{crypto.current_price}</Text>
-        </View>
-      ))}
-    </View>
+    <ScrollView>
+      <View>
+        {cryptocurrencies.map((crypto) => (
+          <View key={crypto.id} style={styles.container}>
+            <Image
+              style={styles.image}
+              source={{ uri: crypto.image }}
+              />
+            <Text>{crypto.name}</Text>
+            <Text>{crypto.current_price}$</Text>
+          </View>
+        ))}
+      </View>
+    </ScrollView>  
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 40,
+  },
+  image: {
+    width: 90,
+    height: 90,
+    borderRadius: 9,
+    margin: 10,
+  },
+});
 
 export default CryptoList;
